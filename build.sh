f246ccfb6ffa4371e538d603e981153704c46b8b
tb <travisbikkle@proton.me>
2024-09-26 19:11:11 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
+current_dir=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+docker compose -f "${current_dir}"/docker-compose.yaml build
 
-docker compose -f ./docker-compose.yaml build
+target_dir=${current_dir}/build
+if [[ -n ${BUILD_DIR} ]];then
+    target_dir=${BUILD_DIR}
+fi
+
+docker save easy-dev/offline-coder:latest > "${target_dir}"/offline-coder.tar
